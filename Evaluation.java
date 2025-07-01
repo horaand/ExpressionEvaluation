ca68e9d3e7308eed00cbbccb15ff7b3d169163ad
horaand <112804361+horaand@users.noreply.github.com>
2025-07-01 19:08:40 +0200

    Update Evaluation.java

--- Evaluation.java
+++ Evaluation.java
@@ -4,6 +4,6 @@ public class Evaluation {
     public static void main(String[] args) {
         System.out.println("Test Data:");
 
-        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println((32.5 * 4.5 - 56.5 * 56.5) / (54.5 - 2.5));
     }
 }
